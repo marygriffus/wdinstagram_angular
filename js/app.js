4517e71980daf4adc34203674f5d63442e9c24cb
Mary Griffus <mary.e.shultz@gmail.com>
2016-05-02 23:04:55 -0400

    add show controller, still not operational

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@
   angular
     .module("wdinstagram", ["ui.router"])
     .config(['$stateProvider', RouterFunction])
-    .controller("instagram_controller", InstagramController)
     .controller("WDIGIndexController", WDIGIndexController)
+    .controller("WDIGShowController", WDIGShowController)
+    .factory("InstagramFactory", InstagramFactory)
 
     function RouterFunction($stateProvider){
       $stateProvider
@@ -14,39 +15,41 @@
         templateUrl: "js/index.html",
         controller: "WDIGIndexController",
         controllerAs: "indexVm"
-      });
+      })
+      .state("wdinstagramShow", {
+        url: "/instagrams/:id",
+        templateUrl: "js/show.html",
+        controller: "WDIGShowController",
+        controllerAs: "showVm"
+      })
     }
 
 
-    function InstagramController(){
-      var vm = this;
-      vm.addGram = function() {
-        vm.grams.push({photo_url: this.photo_url, author: this.author, body: this.body});
-        vm.photo_url = '';
-        vm.author = '';
-        vm.body = '';
-      };
-      vm.update = function(){
-        vm.data[i] = {photo_url: this.photo_url, author: this.author, body: this.body};
-      };
-      vm.delete = function(){
-        vm.data.splice(i, 1);
-      };
-    };
-
-    function WDIGIndexController(){
-      var indexVm = this;
-      indexVm.grams = [
+    function InstagramFactory(){
+      var igf = this;
+      igf.grams = [
         {photo_url: "http://i.telegraph.co.uk/multimedia/archive/03597/POTD_chick_3597497k.jpg", author: "Billy", body: "woo, picture"},
         {photo_url: "http://media2.s-nbcnews.com/j/msnbc/components/video/__new/ss-2015-yip-31-vid-tease.nbcnews-ux-1080-600.jpg", author: "Shannon", body: "i also took a picture"},
         {photo_url: "https://pbs.twimg.com/profile_images/625105219102318592/M5Zq8Fvx_400x400.jpg", author: "Ny'jae", body: "this is an eagle, amazing"}
       ];
+      return igf.grams;
+    };
+
+    function WDIGIndexController(InstagramFactory){
+      var indexVm = this;
+      indexVm.grams = InstagramFactory
       indexVm.addGram = function() {
-        indexVm.grams.push({photo_url: this.photo_url, author: this.author, body: this.body});
+        InstagramFactory.grams.push({photo_url: this.photo_url, author: this.author, body: this.body});
         indexVm.photo_url = '';
         indexVm.author = '';
         indexVm.body = '';
       };
+    };
 
+    WDIGShowController.$inject = ["$stateParams"]
+    function WDIGShowController($stateParams, InstagramFactory){
+      var showVm = this;
+      showVm.grams = InstagramFactory
+      showVm.gram = showVm.grams[$stateParams.id]
     };
 })();
